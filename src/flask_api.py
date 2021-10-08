035edea95054412f20a5d3651d35688eafac6220
= <macaquedev@gmail.com>
2021-10-08 11:55:02 +0100

    Added flask api

---
+++ src/flask_api.py
@@ -0,0 +1,21 @@
+from chatbot import Chatbot
+
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+cb = Chatbot()
+
+
+class API(Resource):
+    def get(self, question):
+        return {"data": cb.get_response(question)}
+
+
+api.add_resource(API, "/<string:question>")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
